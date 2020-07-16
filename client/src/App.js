44165b22cec34236ff25ec131e1a2b969324192d
mastrero <arunaiekhil@gmail.com>
2020-07-16 11:51:05 +0530

    Temporarily removed Dark Mode

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Hero from "./Components/Hero";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { Login, Signup } from "./Components/LoginSignup";
-import { SwitchTheme } from "./UI/SwitchTheme";
 import { Navbar, Dashboard, Cart } from "./Components";
 import { AuthService } from './services'
 
@@ -10,8 +9,6 @@ const App = () => {
 	const [user, setUser] = React.useState();
 
 	React.useEffect(() => {
-		(localStorage.getItem("theme") === "light" || !localStorage.getItem("theme")) && document.body.classList.remove("dark-theme");
-		localStorage.getItem("theme") === "dark" && document.body.classList.add("dark-theme");
 		const token = AuthService.getCurrentUser();
 		token && !user && setUser(token);
 	}, [user]);
@@ -19,16 +16,6 @@ const App = () => {
 	return (
 		<div style={{ background: "var(--main-bg)" }}>
 			{user ? <UserRoutes /> : <PublicRoutes />}
-			<div className='switchThemeWrapper'>
-				<SwitchTheme
-					boxStyles={{
-						borderColor: "var(--font)",
-						borderWidth: "1px 1px 0 0",
-						borderStyle: "solid",
-						borderRadius: "0 20px 0 0",
-					}}
-				/>
-			</div>
 		</div>
 	);
 };
